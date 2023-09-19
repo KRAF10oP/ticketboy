c24284babcf3a73479c500f2ba7875f9d6548fbb
KRAF10oP <88928954+KRAF10oP@users.noreply.github.com>
2023-09-19 18:52:57 +0530

    Add files via upload

---
+++ variables/variables.js
@@ -0,0 +1,10 @@
+module.exports = bot => {
+    bot.variables({
+        color: "00ffbe", // you can change the embed color
+        tcs: "1123856602332659712", // put ticket category id here
+        tsm: "for calling suport use this button", // you can change this value
+        tsn: "1", // don't change this 
+        reason: "love", // don't change this ⚡
+        save: "1123858402662162462" // put sace channel id here and in save.js file
+    });
+}
